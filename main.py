e63d342b8da4fb7ec3905ac87c704eed4bfcb525
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-07 20:09:55 +0200

    Add non-followers

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from src.tables import createTable
 
 
 def main():
-    followers = createTable('Followers', getFollowers())
-    followings = createTable('Followings', getFollowings())
+    followers_list = getFollowers()
+    followings_list = getFollowings()
+    dont_follow_back = []
+
+    for following in followings_list:
+        if following not in followers_list:
+            dont_follow_back.append(following)
+
+    followers = createTable('Followers', followers_list)
+    followings = createTable('Followings', followings_list)
+    non_followers = createTable('Non-Followers', dont_follow_back)
 
     followers.display()
     followings.display()
+    non_followers.display()
 
 
 if __name__ == '__main__':
